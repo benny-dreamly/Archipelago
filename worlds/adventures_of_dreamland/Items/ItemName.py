af702fc0d3f06bf7b0a3fa9f2f89dae26502afa4
Benny Dreamly <bgaming0277@gmail.com>
2024-12-07 14:47:10 -0700

    Item Names from Manual 1.0

---
+++ worlds/adventures_of_dreamland/Items/ItemName.py
@@ -0,0 +1,54 @@
+# Puzzle Pieces
+
+puzzle_piece_one = "Puzzle Piece 1"
+puzzle_piece_two = "Puzzle Piece 2"
+puzzle_piece_three = "Puzzle Piece 3"
+puzzle_piece_four = "Puzzle Piece 4"
+
+# Puzzles
+
+empty_puzzle = "Puzzle"
+partially_empty_puzzle = "Puzzle (1/4)"
+half_finished_puzzle = "Puzzle (2/4)"
+almost_finished_puzzle = "Puzzle (3/4)"
+finished_puzzle = "Puzzle (4/4)"
+
+# Hints
+
+hint_one = "Hint 1"
+scroll_hint = "Scroll Hint"
+gold_bar = "Gold Bar"
+fragment_clue = "Fragment Clue"
+hint_three = "Hint 3"
+
+# Clues
+
+clue_one = "Clue 1"
+clue_one_part_two = "Clue 1-2"
+clue_two = "Clue 2"
+
+# Hint Fragments
+
+hint_fragment_a = "Hint Fragment A"
+hint_fragment_b = "Hint Fragment B"
+hint_fragment_c = "Hint Fragment C"
+hint_fragment_d = "Hint Fragment D"
+hint_fragment_e = "Hint Fragment E"
+hint_fragment_f = "Hint Fragment F"
+hint_fragment_g = "Hint Fragment G"
+hint_fragment_h = "Hint Fragment H"
+hint_fragment_i = "Hint Fragment I"
+hint_fragment_j = "Hint Fragment J"
+hint_fragment_k = "Hint Fragment K"
+hint_fragment_l = "Hint Fragment L"
+hint_fragment_m = "Hint Fragment M"
+
+# other progression items
+
+key = "Key"
+glue_stick = "Glue Stick"
+broom = "Broom"
+empty_bucket = "Bucket"
+filled_bucket = "Bucket with Water"
+magnifying_glass = "Magnifying Glass"
+lighter = "Lighter"
