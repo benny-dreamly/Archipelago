c71c481f3303307eec93e6222f676bbd50979e43
Benny Dreamly <bgaming0277@gmail.com>
2025-10-07 21:39:51 -0600

    okay I was using the wrong thing. self.slot is what i need.

--- worlds/sims4/Client.py
+++ worlds/sims4/Client.py
@@ -178,7 +178,7 @@ class SimsContext(SuperContext):
                 'seed_name': self.seed_name,
                 'goal': self.goal,
                 'career': self.career,
-                'slot': self.slot_info[self.slot][3]
+                'slot': self.slot
             }
             print_json(payload, 'connection_status.json', self)
 
