1677b51693a9ba41b5b5fbbc750f18ffbc6e1e93
Benny Dreamly <bgaming0277@gmail.com>
2026-09-06 12:11:45 -0600

    redo the imports to be clearer

--- worlds/taylorswift/__init__.py
+++ worlds/taylorswift/__init__.py
@@ -9,10 +9,10 @@ from typing import Any
 
 from worlds.AutoWorld import World
 from Utils import visualize_regions
-from . import Items as taylor_swift_items
-from . import Locations as taylor_swift_locations
-from . import Regions as taylor_swift_regions
-from . import Rules as taylor_swift_rules
+from .Items import TaylorSwiftItem, ITEM_NAME_TO_ID, create_all_items, create_item_with_correct_classification, get_random_filler_item_name
+from .Locations import LOCATION_NAME_TO_ID, create_all_locations
+from .Regions import create_and_connect_regions
+from .Rules import set_all_rules, set_completion_condition
 from .Options import TaylorSwiftOptions
 from .Web import TaylorSwiftWeb
 from .UT import UTMixin
@@ -28,12 +28,12 @@ class TaylorSwiftWorld(World, UTMixin):
     options_dataclass = TaylorSwiftOptions
     options: TaylorSwiftOptions
 
-    item_name_to_id = taylor_swift_items.ITEM_NAME_TO_ID
-    location_name_to_id = taylor_swift_locations.LOCATION_NAME_TO_ID
+    item_name_to_id = ITEM_NAME_TO_ID
+    location_name_to_id = LOCATION_NAME_TO_ID
 
     def create_regions(self) -> None:
-        taylor_swift_regions.create_and_connect_regions(self)
-        taylor_swift_locations.create_all_locations(self)
+        create_and_connect_regions(self)
+        create_all_locations(self)
         # visualize_regions(
         #     self.get_region("Menu"),
         #     "taylorswift.puml",
@@ -42,17 +42,17 @@ class TaylorSwiftWorld(World, UTMixin):
         # )
 
     def set_rules(self) -> None:
-        taylor_swift_rules.set_all_rules(self)
-        taylor_swift_rules.set_completion_condition(self)
+        set_all_rules(self)
+        set_completion_condition(self)
 
-    def create_item(self, name: str) -> taylor_swift_items.TaylorSwiftItem:
-        return taylor_swift_items.create_item_with_correct_classification(self, name)
+    def create_item(self, name: str) -> TaylorSwiftItem:
+        return create_item_with_correct_classification(self, name)
 
     def create_items(self) -> None:
-        taylor_swift_items.create_all_items(self)
+        create_all_items(self)
 
     def get_filler_item_name(self) -> str:
-        return taylor_swift_items.get_random_filler_item_name(self)
+        return get_random_filler_item_name(self)
 
     def fill_slot_data(self) -> Mapping[str, Any]:
         return {
